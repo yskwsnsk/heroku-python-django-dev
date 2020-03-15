05aac08f20830cb477d1cc91aaad1215f4b2f61f
吉川　峻策 <yoshikawashunsaku@maedanagisa-no-MacBook-Air.local>
2020-03-15 19:01:49 +0900

    a

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', static.index , name='index'),
+    path('',views.index, name = 'index'),
     path('hello/', include('hello.urls')),
     path('admin/', admin.site.urls),
 ]
