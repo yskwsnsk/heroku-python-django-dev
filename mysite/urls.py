4987d075cbc12537c2ed66f8db1b007a2f89c982
吉川　峻策 <yoshikawashunsaku@maedanagisa-no-MacBook-Air.local>
2020-03-15 19:07:58 +0900

    a

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path('',views.index, name = 'index'),
+    path('', views.index, name = 'index'),
     path('hello/', include('hello.urls')),
     path('admin/', admin.site.urls),
 ]
