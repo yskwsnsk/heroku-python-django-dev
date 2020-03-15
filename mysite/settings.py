d6f2c6f5d5cf57554f667092d7435c7c2291f72a
吉川　峻策 <yoshikawashunsaku@maedanagisa-no-MacBook-Air.local>
2020-03-15 17:21:59 +0900

    a

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*xxqqaix#zel^3p7ks8h_=h&*t!_o=*d3(pgooj6^7=0pegh7h'
+SECRET_KEY = "{{ secret_key }}"
+# SECRET_KEY = '*xxqqaix#zel^3p7ks8h_=h&*t!_o=*d3(pgooj6^7=0pegh7h'
+# local用のためコメントアウト
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
