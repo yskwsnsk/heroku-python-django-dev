26b0af8738f541ca1b68ff6674b9f19a48fd5069
吉川　峻策 <yoshikawashunsaku@maedanagisa-no-MacBook-Air.local>
2020-03-15 14:53:37 +0900

    a

--- hello/settings.py
+++ hello/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = '{{ project_name }}.urls'
+ROOT_URLCONF = 'hello.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
+WSGI_APPLICATION = 'hello.wsgi.application'
 
 
 # Database
